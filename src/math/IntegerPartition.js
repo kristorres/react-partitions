15cf3d21d446ec2cc00bd9ea2c9942cba914254f
Kris Torres <kristorresdayo@gmail.com>
2019-10-25 09:45:40 -0700

    Modify generateRandom to accept part restrictions
    I modified `IntegerPartition.generateRandom` so that the user can also
    specify any of the following restrictions on the generated random
    integer partition λ:
    
      * Even parts only
      * Odd parts only

--- src/math/IntegerPartition.js
+++ src/math/IntegerPartition.js
@@ -1,5 +1,28 @@
 const {PI: π, floor, log, random, sqrt} = Math;
 
+const partitionRestrictions = {
+    none: {
+        allowPart: (part) => true,
+        validateWeight: (weight) => true
+    },
+    even: {
+        allowPart: (part) => part % 2 === 0,
+        validateWeight: (weight) => weight % 2 === 0
+    },
+    odd: {
+        allowPart: (part) => part % 2 === 1,
+        validateWeight: (weight) => true
+    }
+};
+
+const getPartitionRestriction = (partRestriction) => {
+    const partitionRestriction = partitionRestrictions[partRestriction];
+    if (partitionRestriction === undefined) {
+        throw new Error(`Invalid part restriction: "${partRestriction}".`);
+    }
+    return partitionRestriction;
+};
+
 const validatePositiveInteger = (n) => {
     if (
         typeof n !== "number"
@@ -12,13 +35,17 @@ const validatePositiveInteger = (n) => {
 };
 
 const IntegerPartition = {
-    generateRandom: (n) => {
+    generateRandom: (n, partRestriction = "none") => {
         validatePositiveInteger(n);
+        const restriction = getPartitionRestriction(partRestriction);
+        if (restriction.validateWeight(n) === false) {
+            throw new Error(`Invalid value for n due to restriction: ${n}.`);
+        }
         let λ = [];
         while (true) {
             for (let i = 1; i <= n; i += 1) {
                 const U = random();
-                if (U > 0) {
+                if (restriction.allowPart(i) && U > 0) {
                     const Zᵢ = floor((-sqrt(6 * n) * log(U)) / (π * i));
                     for (let j = 0; j < Zᵢ; j += 1) {
                         λ.push(i);
