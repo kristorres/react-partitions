b7596b2fab1e94ba7689359dc9f2b89ebf2c7988
Kris Torres <kristorresdayo@gmail.com>
2019-10-29 21:15:07 -0700

    Add the custom useMedia hook
    The `useMedia` hook is demoed at React Conf 2018 by @ryanflorence
    (https://www.youtube.com/watch?v=dpw9EHDh2bM).

---
+++ src/hooks.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+
+const useMedia = (query) => {
+    const [matches, setMatches] = useState(window.matchMedia(query).matches);
+    useEffect(
+        () => {
+            const media = window.matchMedia(query);
+            if (media.matches !== matches) {
+                setMatches(media.matches);
+            }
+            const listener = () => {
+                setMatches(media.matches);
+            };
+            media.addListener(listener);
+            return () => {
+                media.removeListener(listener);
+            };
+        },
+        [query]
+    );
+    return matches;
+};
+
+export {
+    useMedia
+};
