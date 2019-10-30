8ddba8431594278106f1984c69e694fdd7573ef6
Kris Torres <kristorresdayo@gmail.com>
2019-10-30 11:46:58 -0700

    Create a content view with an empty GUI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import FlexBox from "./FlexBox.js";
 import Footer from "./Footer.js";
-import HelloWorld from "./HelloWorld.js";
 import NavigationBar from "./NavigationBar.js";
+import ContentView from "./views/ContentView/index.js";
 
 function Layout() {
     return (
         <FlexBox style={{minHeight: "100vh"}}>
             <NavigationBar/>
-            <HelloWorld/>
+            <ContentView/>
             <Footer/>
         </FlexBox>
     );
---
+++ src/components/views/ContentView/GUI.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const styles = {
+    root: {
+        backgroundColor: "#F5F5F5",
+        width: "100%",
+        minHeight: 1000
+    }
+};
+
+function GUI() {
+    return (
+        <div style={styles.root}></div>
+    );
+}
+
+export default GUI;
