d383d818f4d8aa75c1e9bd387940c865b74a80e3
Kris Torres <kristorresdayo@gmail.com>
2019-11-02 21:59:28 -0700

    Implement the IntegerTextField control component

---
+++ src/components/controls/IntegerTextField.js
@@ -0,0 +1,111 @@
+import {MDCTextField} from "@material/textfield";
+import PropTypes from "prop-types";
+import React from "react";
+
+import {useComponentDidMount} from "../../hooks.js";
+
+function IntegerTextField({label, value, setValue, min, max, width}) {
+
+    const id = label.replace(/ /g, "-").toLowerCase();
+
+    const styles = {
+        root: {
+            width
+        },
+        input: {
+            textAlign: "right"
+        }
+    };
+
+    const integerValueCanBeNegative = (
+        (min !== undefined && min < 0) || (max !== undefined && max < 0)
+    );
+
+    const validInputRegex = (integerValueCanBeNegative) ? /^[-]?\d*$/ : /^\d*$/;
+
+    const handleChange = (event) => {
+        const newStringValue = event.target.value;
+        if (
+            validInputRegex.test(newStringValue) === false
+            || newStringValue.startsWith("-0")
+            || (newStringValue.startsWith("0") && newStringValue.length > 1)
+        ) {
+            return;
+        }
+        const newIntegerValue = parseInt(newStringValue, 10);
+        if (isNaN(newIntegerValue)) {
+            setValue(newStringValue);
+            return;
+        }
+        if (
+            (min !== undefined && newIntegerValue < min)
+            || (max !== undefined && newIntegerValue > max)
+        ) {
+            return;
+        }
+        setValue(newStringValue);
+    };
+
+    const handleKeyPress = (event) => {
+        const {key, target} = event;
+        if (key !== "ArrowUp" && key !== "ArrowDown") {
+            return;
+        }
+        event.preventDefault();
+        const currentIntegerValue = parseInt(target.value, 10) || min || 0;
+        const increment = (key === "ArrowUp") ? 1 : -1;
+        const newIntegerValue = currentIntegerValue + increment;
+        if (min !== undefined && newIntegerValue < min) {
+            setValue(min.toString());
+            return;
+        }
+        if (max !== undefined && newIntegerValue > max) {
+            setValue(max.toString());
+            return;
+        }
+        setValue(newIntegerValue.toString());
+    };
+
+    useComponentDidMount(
+        () => {
+            new MDCTextField(document.querySelector(`.${id}`));
+        }
+    );
+
+    return (
+        <div
+            className={`mdc-text-field mdc-text-field--outlined ${id}`}
+            style={styles.root}
+        >
+            <input
+                type="text"
+                id={id}
+                className="mdc-text-field__input"
+                style={styles.input}
+                value={value}
+                onChange={handleChange}
+                onKeyDown={handleKeyPress}
+            />
+            <div className="mdc-notched-outline">
+                <div className="mdc-notched-outline__leading"></div>
+                <div className="mdc-notched-outline__notch">
+                    <label htmlFor={id} className="mdc-floating-label">
+                        {label}
+                    </label>
+                </div>
+                <div className="mdc-notched-outline__trailing"></div>
+            </div>
+        </div>
+    );
+}
+
+IntegerTextField.propTypes = {
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    setValue: PropTypes.func.isRequired,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+export default IntegerTextField;
