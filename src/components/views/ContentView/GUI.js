0e568b12ff430231148c761661b7a569b1cc5b0c
Kris Torres <kristorresdayo@gmail.com>
2019-11-04 21:30:19 -0800

    Add a text field for the partition size
    This commit covers “subtask” 1 of issue #3.

--- src/components/views/ContentView/GUI.js
+++ src/components/views/ContentView/GUI.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+
+import FlexBox from "../../FlexBox.js";
+import IntegerTextField from "../../controls/IntegerTextField.js";
+import {useMedia} from "../../../hooks.js";
 
 const styles = {
     root: {
         backgroundColor: "#F5F5F5",
         width: "100%",
         minHeight: 1000
+    },
+    controlPanel: {
+        padding: "24px 0"
+    },
+    control: {
+        width: 240,
+        margin: 12
     }
 };
 
 function GUI() {
+    const [partitionSizeString, setPartitionSizeString] = useState("");
+    const windowWidthIsAtLeastSmall = useMedia("(min-width: 600px)");
     return (
-        <div style={styles.root}></div>
+        <div style={styles.root}>
+            <FlexBox
+                direction={windowWidthIsAtLeastSmall ? "row" : "column"}
+                justifyContent="center"
+                alignItems="center"
+                style={styles.controlPanel}
+            >
+                <IntegerTextField
+                    style={styles.control}
+                    label="Partition Size"
+                    value={partitionSizeString}
+                    setValue={setPartitionSizeString}
+                    min={1}
+                />
+            </FlexBox>
+        </div>
     );
 }
 
