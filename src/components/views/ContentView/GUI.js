2ef8592049259baa46a035681cc27cbf275d0425
Kris Torres <kristorresdayo@gmail.com>
2019-11-07 22:43:31 -0800

    Add a dropdown for the bijection
    This commit covers “subtask” 2 of issue #3.

--- src/components/views/ContentView/GUI.js
+++ src/components/views/ContentView/GUI.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 import FlexBox from "../../FlexBox.js";
 import IntegerTextField from "../../controls/IntegerTextField.js";
+import SelectMenu from "../../controls/SelectMenu.js";
 import {useMedia} from "../../../hooks.js";
 
 const styles = {
@@ -16,11 +17,30 @@ const styles = {
     control: {
         width: 240,
         margin: 12
+    },
+    bijectionDescription: {
+        margin: 0
     }
+};
+
+const bijectionDescriptions = {
+    "Strike-slip": (
+        "Works on most partitions."
+    ),
+    "Shred-and-stretch": (
+        "Even partition ↦ Even partition"
+    ),
+    "Cut-and-stretch": (
+        "Self-conjugate partition ↦ Partition with distinct odd parts"
+    ),
+    "Sylvester/Glaisher": (
+        "Odd partition ↦ Partition with distinct parts"
+    )
 };
 
 function GUI() {
     const [partitionSizeString, setPartitionSizeString] = useState("");
+    const [bijection, setBijection] = useState(null);
     const windowWidthIsAtLeastSmall = useMedia("(min-width: 600px)");
     return (
         <div style={styles.root}>
@@ -37,6 +57,22 @@ function GUI() {
                     setValue={setPartitionSizeString}
                     min={1}
                 />
+                <SelectMenu
+                    style={styles.control}
+                    label="Bijection"
+                    value={bijection}
+                    options={Object.keys(bijectionDescriptions)}
+                    setValue={setBijection}
+                    width={240}
+                />
+                <div style={styles.control}>
+                    <p
+                        className="mdc-typography--body1"
+                        style={styles.bijectionDescription}
+                    >
+                        {bijectionDescriptions[bijection] || ""}
+                    </p>
+                </div>
             </FlexBox>
         </div>
     );
